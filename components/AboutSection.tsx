be70d31c5bbddd727af4f6281f1cfff7b263874c
almerav <almera.valladolid@wvsu.edu.ph>
2025-07-01 15:34:52 +0800

    modified: deleted the duplicate import

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import Image from 'next/image'
 import React from 'react'
 import SkillCard from '@/components/SkillCard'
-import React from 'react'
 import {
   User,
   Briefcase,
